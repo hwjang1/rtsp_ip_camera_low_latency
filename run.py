8ab65acdf4befa940239dd994a2896e35417dbdd
hwjang <hwjang@euclidsoft.co.kr>
2022-12-01 17:09:23 +0900

    initial

--- run.py
+++ run.py
@@ -46,4 +46,5 @@ if __name__ == "__main__":
         FPS = int(1. / (terminate_t - start_t))
         cv2.putText(image, 'FPS : %s' % (str(FPS), (20, 20), 1, 1, (255, 255, 255), 2, cv2.LINE_AA))
         cv2.imshow("RTSP_IP_CAMERA_LOW_LATENCY", image)
-        cv2.waitKey(1)
+
+    cv2.destroyAllWindows()
